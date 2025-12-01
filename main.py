b53fc2b711d73d3d8cc7908574d2bea037a66f47
unknown <hello@gmail.com>
2025-12-01 14:06:35 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def add_lists(a: list,b: list) -> list:
+    """_summary_
+
+    Args:
+        a (list): _description_
+        b (list): _description_
+
+    Returns:
+        list: _description_
+    """
+    return a+b
