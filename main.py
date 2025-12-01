0ed296a6d36a3b8f185a1921e721171b20506ca4
unknown <hello@gmail.com>
2025-12-01 14:14:13 +0100

    Removed docstring

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 def add_lists(a: list,b: list) -> list:
-    """_summary_
-
-    Args:
-        a (list): _description_
-        b (list): _description_
-
-    Returns:
-        list: _description_
-    """
+   
     return a+b
